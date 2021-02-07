c9657b61982b70dba0d2ced11074780aabed071c
Tierra Rogers <tierragore@yahoo.com>
2021-02-07 12:17:47 -0500

    Updating Files

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 // Assignment Code
-var charset ="0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()?/<>:;-=-+{}[];
-var passwordLength=8
-var password="";
-for(var i=0;i<passwordLength;i++){
-  var randonnum = Math.floor(Math.random() *charset.lenght);
-  password += charset.substring(randomnum, random+1); 
+var length = Number(prompt("How many characters would you like your password to be?"));
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+
+var uppers = confirm("Would you like to use uppercase letters?");
+var lowers = confirm("Would you like to use lowercase letters?");
+var numbers = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use special characters?");
+
+while (!uppers && !lowers && !numbers && !symbols) {
+  alert("You must select at least one character type!");
+  uppers = confirm("Would you like to use uppercase letters?");
+  lowers = confirm("Would you like to use lowercase letters?");
+  numbers = confirm("Would you like to use numbers?");
+  symbols = confirm("Would you like to use special characters?");
 }
-document.getElementById("input").vaule=password;
 
+window.addEventListener('load', function() {
+  generateNewPassword();
+});
 
+function generateNewPassword() {
+  var password = "";
+
+  var allowed = {};
+  if (uppers) password += rando(allowed.uppers = "QWERTYUIOPASDFGHJKLZXCVBNM");
+  if (lowers) password += rando(allowed.lowers = "qwertyuiopasdfghjklzxcvbnm");
+  if (numbers) password += rando(allowed.numbers = "1234567890");
+  if (symbols) password += rando(allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
+
+  for (var i = password.length; i < length; i++) password += rando(rando(allowed).value);
+
+  document.getElementById("password").value = randoSequence(password).join("");
+}
 
 var generateBtn = document.querySelector("#generate");
 
-};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -25,4 +47,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-const password = generateBtn()
