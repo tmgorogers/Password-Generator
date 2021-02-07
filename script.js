a4ec21ba4e5361b26d2fbcf5065f6c2a1f6da33f
Tierra Rogers <tierragore@yahoo.com>
2021-02-06 23:33:46 -0500

    Updating Files

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
+var charset ="0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()?/<>:;-=-+{}[];
+var passwordLength=8
+var password="";
+for(var i=0;i<passwordLength;i++){
+  var randonnum = Math.floor(Math.random() *charset.lenght);
+  password += charset.substring(randomnum, random+1); 
+}
+document.getElementById("input").vaule=password;
+
+
+
 var generateBtn = document.querySelector("#generate");
 
+};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +24,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+const password = generateBtn()
